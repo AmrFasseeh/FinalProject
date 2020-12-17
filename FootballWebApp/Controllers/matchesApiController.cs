2c5f04b78a10fda8daa3009b3917dc08cd6f83fd
MahmoudGhanem93 <mahmoudtifaghanem@gmail.com>
2020-12-17 19:06:12 +0200

    Apis

---
+++ FootballWebApp/Controllers/matchesApiController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FootballWebApp.Models;
+
+namespace FootballWebApp.Controllers
+{
+    public class matchesApiController : ApiController
+    {
+        private FootballDB db = new FootballDB();
+
+        // GET: api/matchesApi
+        public IQueryable<match> Getmatches()
+        {
+            return db.matches;
+        }
+
+        // GET: api/matchesApi/5
+        [ResponseType(typeof(match))]
+        public IHttpActionResult Getmatch(int id)
+        {
+            match match = db.matches.Find(id);
+            if (match == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(match);
+        }
+
+        // PUT: api/matchesApi/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putmatch(int id, match match)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != match.match_id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(match).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!matchExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/matchesApi
+        [ResponseType(typeof(match))]
+        public IHttpActionResult Postmatch(match match)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.matches.Add(match);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = match.match_id }, match);
+        }
+
+        // DELETE: api/matchesApi/5
+        [ResponseType(typeof(match))]
+        public IHttpActionResult Deletematch(int id)
+        {
+            match match = db.matches.Find(id);
+            if (match == null)
+            {
+                return NotFound();
+            }
+
+            db.matches.Remove(match);
+            db.SaveChanges();
+
+            return Ok(match);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool matchExists(int id)
+        {
+            return db.matches.Count(e => e.match_id == id) > 0;
+        }
+    }
+}
---
+++ FootballWebApp/Controllers/tagsApiController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FootballWebApp.Models;
+
+namespace FootballWebApp.Controllers
+{
+    public class tagsApiController : ApiController
+    {
+        private FootballDB db = new FootballDB();
+
+        // GET: api/tagsApi
+        public IQueryable<tag> Gettags()
+        {
+            return db.tags;
+        }
+
+        // GET: api/tagsApi/5
+        [ResponseType(typeof(tag))]
+        public IHttpActionResult Gettag(int id)
+        {
+            tag tag = db.tags.Find(id);
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tag);
+        }
+
+        // PUT: api/tagsApi/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Puttag(int id, tag tag)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != tag.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(tag).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!tagExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/tagsApi
+        [ResponseType(typeof(tag))]
+        public IHttpActionResult Posttag(tag tag)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.tags.Add(tag);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = tag.id }, tag);
+        }
+
+        // DELETE: api/tagsApi/5
+        [ResponseType(typeof(tag))]
+        public IHttpActionResult Deletetag(int id)
+        {
+            tag tag = db.tags.Find(id);
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            db.tags.Remove(tag);
+            db.SaveChanges();
+
+            return Ok(tag);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool tagExists(int id)
+        {
+            return db.tags.Count(e => e.id == id) > 0;
+        }
+    }
+}
