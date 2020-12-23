087a7d71d05da4a36f4fd344a8640a13924f48be
Amr Fasseeh <amr_m_fasseeh@hotmail.com>
2020-12-23 21:46:15 +0200

    Some Final edits

--- FootballWebApp/Controllers/HomeController.cs
+++ FootballWebApp/Controllers/HomeController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FootballWebApp.Models;
 
 namespace FootballWebApp.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
-        [Authorize]
+        
+
+        FootballDB db = new FootballDB();
+        
         public ActionResult Index()
         {
+            ViewBag.matches = db.matches.ToList().Count;
+            ViewBag.leagues = db.leagues.ToList().Count;
+            ViewBag.teams = db.teams.ToList().Count;
+            ViewBag.players = db.players.ToList().Count;
+            ViewBag.goals = db.goals.ToList().Count;
+            ViewBag.yellowcards = db.yellow_cards.ToList().Count;
+            ViewBag.redcards = db.red_cards.ToList().Count;
             return View();
         }
 
@@ -25,6 +37,11 @@ namespace FootballWebApp.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Test()
+        {
             return View();
         }
     }
